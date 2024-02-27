b1aa57c10110242bce934a5d4ff497ab3cd89c8b
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-02-27 17:37:30 +0700

    add Icon

---
+++ src/assets/icon/ShoppingBagIcon.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function ShoppingBagIcon() {
+  return (
+    <svg
+      width='23'
+      height='23'
+      viewBox='0 0 23 23'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M15.333 10.5417V6.70833C15.333 5.69167 14.9291 4.71665 14.2103 3.99776C13.4914 3.27887 12.5163 2.875 11.4997 2.875C10.483 2.875 9.50799 3.27887 8.7891 3.99776C8.07021 4.71665 7.66634 5.69167 7.66634 6.70833V10.5417M4.79134 8.625H18.208L19.1663 20.125H3.83301L4.79134 8.625Z'
+        stroke='#3F3F46'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
--- src/layouts/CustomerNavBar.jsx
+++ src/layouts/CustomerNavBar.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import ToyMartLogo from '../assets/icon/ToyMartLogo';
+import SearchBar from '../components/SearchBar';
+import ThaiFlagIcon from '../assets/icon/ThaiFlagIcon';
 
 export default function CustomerNavBar() {
-  return <div>CustomerNavBar</div>;
+  return (
+    <div className='sticky flex w-full h-[3rem] bg-red-500 items-center'>
+      <div className='flex gap-6  items-center font-semibold'>
+        <ToyMartLogo />
+        <div>New & Featured</div>
+        <div>SERIES</div>
+        <div>MEGA</div>
+        <div>TYPES</div>
+        <div>ACCESSORIES</div>
+      </div>
+      <div className='flex gap-4  items-center font-semibold'>
+        <div className='w-[226px] h-[42px]'>
+          <SearchBar />
+        </div>
+        <div className='flex gap-2 items-center'>
+          <div>
+            <ThaiFlagIcon />
+          </div>
+          <div>TH</div>
+        </div>
+        <div className='flex items-center'>
+          <div className='w-[24px] h-[24px] bg-gray-400 rounded-full'></div>
+          <div>My Account</div>
+        </div>
+      </div>
+    </div>
+  );
 }
