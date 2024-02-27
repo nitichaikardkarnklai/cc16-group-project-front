f62d72c36300e163f3d3379891e1eb94b6597dd5
nitichaikardkarnklai <152477400+nitichaikardkarnklai@users.noreply.github.com>
2024-02-27 11:58:19 +0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import viteLogo from './assets/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+
   // console.log('api', import.meta.env);
+  console.log("testtestset");
   return (
     <>
       <div>
