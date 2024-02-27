212412f44764ef4f867ba97558c66a7a4f69fab4
Mollyngo <mol101604@gmail.com>
2024-02-27 17:36:41 +0700

    Register

--- src/features/Auth/components/LoginForm.jsx
+++ src/features/Auth/components/LoginForm.jsx
@@ -1,5 +1,88 @@
-import React from 'react';
+import { useState } from "react";
+import { toast } from "react-toastify";
+import Input from "../../../components/Input";
+import Button from "../../../components/Button";
+import { useNavigate } from "react-router-dom";
+import validateLogin from "../validators/validateLogin"; // นำเข้าฟังก์ชันตรวจสอบข้อมูล
+import { Link } from "react-router-dom";
+
 
 export default function LoginForm() {
-  return <div>LoginForm</div>;
+  const { login, authUser } = useAuth();
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      validateLogin(input);
+      await login(input);
+      if (authUser) {
+        navigate('/')
+      }
+
+
+    } catch (error) {
+      toast.error("เข้าสู่ระบบไม่สำเร็จ");
+      console.log(error);
+    }
+  };
+
+  const handleChangeInput = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = () => {
+    navigate("/auth/register");
+  };
+
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-4xl font-bold">เข้าสู่ระบบ</h1>
+          </div>
+          <div className="card shrink-0 w-full mt-5 shadow-2xl bg-base-100">
+            <form className="card-body" onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <Input
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  value={input.email}
+                  onChange={handleChangeInput}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <Input
+                  value={input.password}
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                  className="input input-bordered"
+                  onChange={handleChangeInput}
+                />
+              </div>
+              <Button bg="green" type="submit" onClick={handleSubmit}>
+                Login
+              </Button>
+            </form>
+            <button className="btn btn-secondary m-2" onClick={handleRegister}>
+              สมัครสมาชิก
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/features/Auth/components/RegisterForm.jsx
+++ src/features/Auth/components/RegisterForm.jsx
@@ -1,5 +1,77 @@
 import React from 'react';
-
 export default function RegisterForm() {
-  return <div>RegisterForm</div>;
+  const handleSubmit = (e) => {
+    e.target.preventDefault();
+  }
+  return (
+    <div>
+      return (
+      <div className="  bg-white px-6 py-5  ">
+
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">สมัครสมาชิก</h2>
+
+        </div>
+        <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                ชื่อ
+              </label>
+              <div className="mt-2.5">
+                <input
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                นามสกุล
+              </label>
+              <div className="mt-2.5">
+                <input
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="mt-2.5">
+                <input
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                เบอร์โทร
+              </label>
+              <div className="relative mt-2.5">
+                <input>
+                </input>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 items-center">
+            <button type="submit" className="w-full" >
+              สมัครสมาชิก
+            </button>
+          </div>
+        </form >
+        <button type="submit" className="w-full" >
+          ย้อนกลับ
+        </button>
+      </div >
+      )
+    </div>
+  )
 }
