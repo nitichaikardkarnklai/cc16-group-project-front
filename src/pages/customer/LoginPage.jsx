e9ac8b3f180988fb850b3e8b989ed102da3aae75
Mollyngo <mol101604@gmail.com>
2024-02-28 18:27:23 +0700

    change Auth folder name to a

--- src/pages/customer/LoginPage.jsx
+++ src/pages/customer/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../../features/auth/components/LoginForm';
+import LoginForm from '../../features/a/components/LoginForm';
 
 export default function LoginPage() {
   return <div><LoginForm /></div>;
