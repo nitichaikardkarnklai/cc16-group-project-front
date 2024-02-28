a34f6da8b345193d9c184b3a336eaa1bcc9d3d22
nitichaikardkarnklai <152477400+nitichaikardkarnklai@users.noreply.github.com>
2024-02-28 15:01:37 +0700

    Delete src/features/Auth directory

--- src/features/Auth/contexts/AuthContext.jsx
+++
@@ -1,8 +0,0 @@
-import React, { createContext, useState } from 'react';
-
-export const AuthContext = createContext();
-
-export default function AuthContextProvider({ children }) {
-  const [loading, setLoading] = useState('');
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
-}
