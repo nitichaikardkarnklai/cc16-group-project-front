6f4fed4956a1187ad25f9b6e88200f55739f0410
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-03-01 11:37:39 +0700

    edit productContainer

--- src/layouts/components/ProductsContainer.jsx
+++ src/layouts/components/ProductsContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProductCard from './ProductCard';
+import ProductCard from './components/ProductCard';
 
 export default function ProductsContainer({ title = 'TITLE' }) {
   return (
