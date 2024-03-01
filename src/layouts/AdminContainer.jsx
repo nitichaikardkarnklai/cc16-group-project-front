8391cd4e8612e3841b975ddac50ebb92fa53fa35
nitichaikardkarnklai <152477400+nitichaikardkarnklai@users.noreply.github.com>
2024-03-01 12:20:34 +0700

    sidebar init

--- src/layouts/AdminContainer.jsx
+++ src/layouts/AdminContainer.jsx
@@ -4,9 +4,9 @@ import AdminSideBar from "../layouts/AdminSideBar"
 
 export default function AdminContainer() {
   return (
-    <>
+    <div className='min-h-screen flex'>
       <AdminSideBar />
       <Outlet />
-    </>
+    </div>
   );
 }
--- src/layouts/AdminSideBar.jsx
+++ src/layouts/AdminSideBar.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import ToyMartLogo from '../assets/icon/ToyMartLogo'
+import Button from '../components/Button'
 
 export default function AdminSideBar() {
     return (
-        <div>AdminSideBar</div>
+        <div className='w-[24%] p-8 bg-grayBg100'>
+            <ToyMartLogo></ToyMartLogo>
+            <div className='flex flex-col mt-24'>
+                <Button textPosition="start" bg="gray">Home</Button>
+                <Button textPosition="start" bg="gray">Chat</Button>
+                <Button textPosition="start" bg="gray">Category Management</Button>
+                <Button textPosition="start" bg="gray">Product Management</Button>
+                <Button textPosition="start" bg="gray">Trend Management</Button>
+                <Button textPosition="start" bg="gray">Customer Management</Button>
+                <Button textPosition="start" bg="gray">Admin Management</Button>
+                <Button textPosition="start" bg="gray">Transaction Monitoring</Button>
+            </div>
+        </div>
     )
 }
