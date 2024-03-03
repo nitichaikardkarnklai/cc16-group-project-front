b4fa9331b3f93308bf60c2d14fa50f43d964e7f5
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-03-03 13:11:13 +0700

    edit joi

--- src/features/user/validations/validate-editProfile.js
+++ src/features/user/validations/validate-editProfile.js
@@ -5,7 +5,8 @@ const editProfileSchema = Joi.object({
   nickName: Joi.string().allow(null),
   phone: Joi.string()
     .allow(null)
-    .regex(/^[0-9]{10}$/)
+    .allow('')
+    .pattern(/^[0-9]{10}$/)
     .messages({ 'string.pattern.base': `Phone number must have 10 digits.` }),
   birthDate: Joi.string().allow(null),
   gender: Joi.string().allow(null),
