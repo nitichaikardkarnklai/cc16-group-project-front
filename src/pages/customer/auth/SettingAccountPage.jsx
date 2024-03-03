7216811682a60d602dc09030d1b2216bf1d8dcae
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-03-03 20:00:08 +0700

    update settingAccountPage

--- src/pages/customer/auth/SettingAccountPage.jsx
+++ src/pages/customer/auth/SettingAccountPage.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import HeadPhoneIcon from '../../../assets/icon/HeadPhoneIcon';
+import MailIcon from '../../../assets/icon/MailIcon';
+import useAuth from '../../../hooks/use-auth';
 
 export default function SettingAccountPage() {
-  return <div>SettingAccountPage</div>;
+  const { authUser } = useAuth();
+
+  return (
+    <div className='flex flex-col gap-6 w-[885px] mx-auto'>
+      <div className='text-center text-3xl font-semibold pt-12'>
+        ACCOUNT SETTING
+      </div>
+      <div>
+        <div className='text-lg font-medium'>EMAIL</div>
+        <input
+          type='text'
+          placeholder={authUser.email}
+          className='input input-bordered w-full '
+          disabled
+        />
+      </div>
+      <div className='border-b pb-6'>
+        <div className='text-lg font-medium'>Subscribe</div>
+        <div className='flex gap-4'>
+          <input type='checkbox' className='toggle' />
+          <div className='text-sm'>
+            Show me newsletters,promotion,events from TOY MART via Email.
+          </div>
+        </div>
+      </div>
+      <div>
+        <HeadPhoneIcon />
+      </div>
+      <div>
+        <div className='text-lg font-medium'>Online customer service</div>
+        <div className='text-sm'>
+          Click the customer service icon to start talking with us,
+          <br /> using either automated or human service.
+        </div>
+        <div className='text-sm'>7 days a week 09:00 - 18:00</div>
+      </div>
+      <div>
+        <MailIcon />
+      </div>
+      <div className='border-b pb-6'>
+        <div className='text-lg font-medium'>Email service</div>
+        <div className='text-sm'>Please contact us at support@toymart.com</div>
+      </div>
+    </div>
+  );
 }
