7de195ecc2ab612aa4a71d4feb4235bdb94e4856
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-03-07 16:08:30 +0700

    update wishlist Page read and delete

--- src/layouts/components/WishListCard.jsx
+++ src/layouts/components/WishListCard.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
-export default function WishListCard() {
+export default function WishListCard({ data, onRemove }) {
+  console.log('data', data);
+
   return (
     <div className='w-[1035px] flex items-center gap-6 p-2 border-b'>
-      <div className='w-[180px] h-[180px] bg-grayBg100'></div>
-      <div className='flex flex-col w-[1035px] gap-4'>
+      <div className='w-[180px] h-[180px] flex-shrink-0 bg-grayBg100 bg-cover overflow-hidden'>
+        <img src={data.products.productCover?.[0].cover} alt='' />
+      </div>
+      <div className='flex flex-col w-full gap-4'>
         <div>
-          <div className='text-2xl font-semibold'>Product Name</div>
-          <div className='text-grayBg300 text-lg'>SingleBox</div>
+          <div className='text-2xl font-semibold'>
+            {data.products.productName}
+          </div>
+          <div className='text-grayBg300 text-lg'>{data.products.brand}</div>
         </div>
-        <div className='flex w-full justify-between'>
-          <div className='text-redHero text-xl font-semibold'>350.-</div>
-          <div className='text-lg font-medium underline'>REMOVE</div>
+        <div className='flex justify-between'>
+          <div className='text-redHero text-xl font-semibold'>
+            {data.products.price} BTH
+          </div>
+          <div
+            onClick={(c) => onRemove(+data.id)}
+            className=' btn bg-transparent border-none shadow-none text-lg font-medium underline'
+          >
+            REMOVE
+          </div>
         </div>
       </div>
     </div>
--- src/pages/customer/auth/WishlistPage.jsx
+++ src/pages/customer/auth/WishlistPage.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import WishListCard from '../../../layouts/components/WishListCard';
+import {
+  addWishlist,
+  removeWishlist,
+  fetchWishlist,
+} from '../../../store/slices/wishlistSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import Spinner from '../../../components/Spinner';
 
 export default function WishlistPage() {
+  const { wishlistItems } = useSelector((store) => store.wishlists);
+  const [toggle, setToggle] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        dispatch(fetchWishlist());
+        console.log(wishlistItems);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [toggle]);
+
+  const removeFromWishlist = async (id) => {
+    console.log('remove from wishlist');
+    await dispatch(removeWishlist(id));
+    await setToggle(!toggle);
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='text-5xl font-semibold pt-24 pb-12'>MY WISHLIST</div>
       <div className='flex flex-col gap-4'>
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
+        {wishlistItems?.map((el, index) => {
+          return (
+            <WishListCard key={index} data={el} onRemove={removeFromWishlist} />
+          );
+        })}
       </div>
     </div>
   );
